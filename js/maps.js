fb1073112f04b0d10cdbdf76526f8b21ea4d5d7f
Craig Stroman <cstroman@gmail.com>
2013-08-06 10:10:03 -0400

    updates

--- js/maps.js
+++ js/maps.js
@@ -1,18 +1,24 @@
-var busLocator = {
+var objBusLocator,
+busLocator = {
   settings: {
+    objBusLocator: '',
     vehicles: '',
-    agencyTag: '',
+    agencyTag: 'mbta',
     lastTime: '',
     map: '',
     mapOptions: '',
     route: '',
     routeNumber: '',
     routeTitle: '',
-    stops: ''
+    stops: '',
+    selectedStop: '',
+    selectedDirection: '',
+    displayNextTime: false
   },
   
   init: function (){
-      var objBusLocator = this;
+      objBusLocator = this;
+     
       objBusLocator.settings.vehicles=new Array();
       objBusLocator.settings.agency_tag='mbta';
       
@@ -29,10 +35,29 @@ var busLocator = {
       $('#getRoute_btn').click(function(){
         objBusLocator.settings.routeNumber = $('#route_select').val();
         if(objBusLocator.settings.routeNumber>=1){
-                objBusLocator.showRoute(objBusLocator.settings.routeNumber);
+          objBusLocator.emptyStops();
+          objBusLocator.showRoute(objBusLocator.settings.routeNumber);
         }else{
           alert("Please select a route to continue.")
         }       
+      });
+      
+      $('#stop_select').change(function(){
+        objBusLocator.settings.selectedStop = $('#stop_select').val();
+        if(objBusLocator.settings.selectedStop>=1){
+                objBusLocator.updateDirectionInfo();
+        }else{
+          alert("Please select a stop to continue.")
+        }         
+      });
+      
+      $('#getDirection_btn').click(function(){
+        objBusLocator.settings.selectedDirection = $('#direction_select').val();
+        if(objBusLocator.settings.selectedDirection!=0){
+                objBusLocator.updateNextBusTime();  
+        }else{
+          alert("Please select a stop to continue.")
+        }             
       });
   },
   
@@ -56,12 +81,12 @@ var busLocator = {
   },
   
   showRoute: function(){
-    var image = 'images/red_stop.png', objBusLocator = this;
+    var image = 'images/red_stop.png', objStops = '', objBusLocator = this;
     $.ajax({
             async: false,
             cache: false,			
             type: 'GET',
-            url: 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=mbta&r='+objBusLocator.settings.routeNumber,
+            url: 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a='+objBusLocator.settings.agencyTag+'&r='+objBusLocator.settings.routeNumber,
             dataType: 'xml',
             success: function(xml) {
                 //Center map on route
@@ -84,6 +109,7 @@ var busLocator = {
                         var lat = $(this).attr('lat');
                         var lon = $(this).attr('lon');
                         var title = $(this).attr('title');
+                        var stopID = $(this).attr('stopId');
                         var contentString = title;
                         var myLatLng = new google.maps.LatLng(lat,lon);
 
@@ -93,6 +119,10 @@ var busLocator = {
                             title: title,
                             icon: image
                         });
+                        
+                        //Create route stop select box
+                        if(typeof stopID !== 'undefined')
+                          objStops += '<option value="'+ stopID + '">'+title+'</option>';
  
         
                         var infowindow = new google.maps.InfoWindow({
@@ -107,19 +137,22 @@ var busLocator = {
                         
                 });
                 
-
+          
                objBusLocator.setBusLocations();//Set initial bus positions
                 
                 setInterval(function(){//Refresh marker locations every 20 seconds
                      objBusLocator.updateBusLocations();
+                     if(objBusLocator.settings.displayNextTime)
+                        objBusLocator.updateNextBusTime();                     
                  }, 20000);
+                
+                $('#stop_select').append(objStops);
             }
     });   
     
   },
   
   setBusLocations: function(){
-    var objBusLocator = this;
       $.ajax({
               async: false,
               cache: false,			
@@ -152,7 +185,7 @@ var busLocator = {
   },
   
   updateBusLocations: function(){
-    var lat = '', lon = '', busId = '', lastTime = '', objBusLocator = this;
+    var lat = '', lon = '', busId = '', lastTime = '';
     $.ajax({
             async: false,
             cache: false,			
@@ -175,8 +208,58 @@ var busLocator = {
                 });
             }
     });    
+  },
+  
+  updateDirectionInfo: function(){
+    var objDirection='';
+    $.ajax({
+            async: false,
+            cache: false,			
+            type: 'GET',
+            url: 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a='+objBusLocator.settings.agencyTag+'&stopId='+objBusLocator.settings.selectedStop,//mbta&stopId=74611
+            dataType: 'xml',
+            success: function(xml){
+              $(xml).find('direction').each(function(i){
+                objDirection+='<option value="'+$(this).attr('title')+'">'+$(this).attr('title')+'</option>';
+              });
+              
+              $('#direction_select').append(objDirection);
+            }
+    });     
+  },
+  
+  updateNextBusTime: function(){
+    var minutes='';
+    $.ajax({
+            async: false,
+            cache: false,			
+            type: 'GET',
+            url: 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a='+objBusLocator.settings.agencyTag+'&stopId='+objBusLocator.settings.selectedStop,//mbta&stopId=74611
+            dataType: 'xml',
+            success: function(xml){
+              $(xml).find('direction').each(function(){
+                  if($(this).attr('title')===objBusLocator.settings.selectedDirection){
+                    minutes=$(this).find('prediction').first().attr('minutes');
+                  }
+              });
+              minutes = minutes >= 1 ? minutes : 0;
+              
+              $('#minutes_until').empty();
+              $('#minutes_until').append(minutes + ' minute(s).');
+              objBusLocator.settings.displayNextTime = true;
+            }
+    });
+  },
+  emptyStops: function(){
+    
+    $('#stop_select').find('option:gt(0)').remove();
+    $('#direction_select').find('option:gt(0)').remove();
+     
+    if(objBusLocator.settings.displayNextTime){
+      $('#minutes_until').empty();
+      objBusLocator.settings.displayNextTime = false;
+    }     
   }
-
 };
 
 
